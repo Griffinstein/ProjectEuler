86b056e1e2e2d01263d8a63b34ec6ff2e2a48d33
Griffinstein <eddmccaffrey@gmail.com>
2014-01-29 14:32:13 +1100

    Reformatting Solution 13
    Removing redundant code

--- src/projecteuler/Problem13.java
+++ src/projecteuler/Problem13.java
@@ -6,22 +6,9 @@
 
 package projecteuler;
 
-import java.lang.Math;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -29,43 +16,19 @@ import java.util.logging.Logger;
  */
 public class Problem13 implements EulerProblem{
 
-    int a = 58496;
-    int b = 93760;
     final static String FILE_NAME = "C:\\Users\\Ed\\Documents\\NetBeansProjects\\ProjectEuler\\src\\projecteuler\\Problem13.txt";
     private int[][] _numbers;
     
     @Override
     public String getSolution() {
-        return "" + test();
+        return "" + LargeSum();
     }
     
-    private int test() {
-        int num = 0;
+    private int LargeSum() {
         int temp = 0;
         int sum;
         
-        //num = a * 150;
-        
         _numbers = new int[150][50];
-        
-        //BufferedReader reader = new BufferedReader(new FileReader("C:\Users\Ed\Documents\NetBeansProjects\ProjectEuler\src\projecteuler\Problem13.txt"));
-        //String line = null;
-        //while ((line = reader.readLine()) != null) {
-        //    System.out.println(line);
-        //}
-        
-        //List<Integer> integers = new ArrayList<Integer>();
-        //Scanner fileScanner = new Scanner(new File(""));
-        //while (fileScanner.hasNextInt()){
-        //    integers.add(fileScanner.nextInt());
-        //}
-        
-        //treat as a small file
-        //List<String> lines = text.readSmallTextFile(FILE_NAME);
-        //log(lines);
-        //lines.add("This is a line added in code.");
-        //text.writeSmallTextFile(lines, FILE_NAME);
-        
         
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_NAME)))
         {
@@ -81,9 +44,7 @@ public class Problem13 implements EulerProblem{
         } 
         
         temp = 0;
-        
-        //int[] A = {5,8,4,9,6};
-        //int[] B = {9,3,7,6,0};
+
         int[] C = new int[50];
         
         for (int i = 49; i>=0; i--){
@@ -94,13 +55,8 @@ public class Problem13 implements EulerProblem{
             C[i] = (sum + temp)%10;
             temp = (int)Math.floor((sum + temp)/10);
         }
-
-        C = C;
-        
         
         return temp;
-        
-        //return 0;
     }
     
     @Override
